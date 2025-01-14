af194636ff2d41302b67033797d5c95c4911ca50
Wojciech Derwisz <derwisz.wojciech@gmail.com>
2025-01-14 23:02:10 +0100

    Add first drawing system based on dots

---
+++ Assets/Scripts/DrawingPath.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.HID;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using CommonUsages = UnityEngine.XR.CommonUsages;
+
+public class DrawingPath : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject whiteboard;
+
+    [SerializeField]
+    private GameObject dot;
+
+    public InputActionReference primaryButtonAction;
+
+    private XRRayInteractor rayInteractor;
+
+    private bool isHovering = false;
+
+
+    private void Awake()
+    {
+        rayInteractor = FindObjectOfType<XRRayInteractor>();
+    }
+
+    public void OnHoverEntered(HoverEnterEventArgs args)
+    {
+        isHovering = true;
+    }
+
+    public void OnHoverExit(HoverExitEventArgs args)
+    {
+        isHovering = false;
+    }
+
+    private void Update()
+    {
+        if (isHovering)
+        {
+            if (primaryButtonAction.action.ReadValue<float>() > 0)
+            {
+                if (rayInteractor.TryGetCurrent3DRaycastHit(out RaycastHit hit) && hit.collider.gameObject == whiteboard)
+                {
+                    Debug.Log($"Hit Point: {hit.point}");
+                    Instantiate(dot, hit.point, Quaternion.Euler(0f, 0f, 90f));
+                }
+            }
+            else
+            {
+                Debug.Log("No hit");
+            }
+        }
+
+    }
+}
