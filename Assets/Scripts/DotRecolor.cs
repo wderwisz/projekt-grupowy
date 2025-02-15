1a4125330816f7f0677e15925b53f1b7c5a4b359
Mytovsky <Mytovskyy@Gmail.com>
2025-02-15 23:12:10 +0100

    ImprovedDrawing
    Coloring need improvement

--- Assets/Scripts/DotRecolor.cs
+++ Assets/Scripts/DotRecolor.cs
@@ -2,43 +2,36 @@ using UnityEngine;
 
 public class DotRecolor : MonoBehaviour
 {
-    [Tooltip("Materia³, który zostanie ustawiony po odwzorowaniu kropki.")]
     public Material coloredMaterial;
 
-    private Material originalMaterial;
+    public Material originalMaterial;
+
     private MeshRenderer meshRenderer;
 
-    // indeks kropek w œcie¿ce
-    [HideInInspector]
-    public int dotIndex = 0;
-
+    // indeks kropek w sciezce
+    [HideInInspector] public int dotIndex = 0;
     private bool isColored = false;
 
     private void Awake()
     {
         meshRenderer = GetComponent<MeshRenderer>();
-        originalMaterial = meshRenderer.material;
     }
 
-    private void OnTriggerEnter(Collider other)
+    // metoda do zmiany koloru kropki
+    public void Recolor()
     {
-        if (!other.CompareTag("Controller")) return;
-
         PathManager pathManager = FindObjectOfType<PathManager>();
-        if (pathManager == null) return;
 
-        // Sprawdz czy kolejna kropka to ta do kolorwania
         if (pathManager.nextDotIndex == dotIndex && !isColored)
         {
             meshRenderer.material = coloredMaterial;
             isColored = true;
-            Debug.Log("Pokolorowano kropkê o indeksie: " + dotIndex);
+            Debug.Log("DotRecolor: Pokolorowano kropkê o indeksie: " + dotIndex);
             pathManager.nextDotIndex++;  // przechodzimy do nastêpnej kropki
         }
-    }
-
-    public bool IsColored()
-    {
-        return isColored;
+        else
+        {
+            Debug.Log($" Warunki do zmiany koloru nie spe³nione dla kropki {dotIndex}");
+        }
     }
 }
--- Assets/Scripts/DrawingPath.cs
+++ Assets/Scripts/DrawingPath.cs
@@ -4,75 +4,121 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class DrawingPath : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject whiteboard;
-
-    [SerializeField]
-    private GameObject dot;
-
-    private Config config;
-
+    [SerializeField] private GameObject whiteboard;
+    [SerializeField] private GameObject dot;
+    [SerializeField] private Config config;
+    [SerializeField] private PathManager pathManager;
     public InputActionReference primaryButtonAction;
 
     private XRRayInteractor rayInteractor;
-
     private bool isHovering = false;
 
-    private PathManager pathManager;
+    // maksymalna odleglosc miedzy kropkami
+    [SerializeField] private float maxDotSpacing = 0.1f;
+    private Vector3 lastDotPosition = Vector3.zero; // pozycja ostatniej kropki
 
     private void Awake()
     {
         rayInteractor = FindObjectOfType<XRRayInteractor>();
-        if (config == null)
-        {
-            config = Resources.Load<Config>("MainConfig");
-            if (config == null)
-                Debug.LogError("Nie uda³o siê za³adowaæ Config z Resources!");
-        }
-    }
 
+    }
 
     private void Update()
     {
-        if (config == null)
-        {
-            Debug.LogError("Config nie zosta³ przypisany!");
-            return; 
-        }
 
+        //tryb rysowania
+        
         if (config.getDrawingMode())
         {
+            //Debug.Log("Tryb rysowania wlaczony");
             if (isHovering)
             {
-                if (primaryButtonAction.action.ReadValue<float>() > 0)  // wcisniecie przycisku
+                if (primaryButtonAction.action.ReadValue<float>() > 0)
                 {
                     if (rayInteractor.TryGetCurrent3DRaycastHit(out RaycastHit hit) && hit.collider.gameObject == whiteboard)
                     {
-                        Debug.Log($"Hit Point: {hit.point}");
-                        GameObject newDot = Instantiate(dot, hit.point, Quaternion.Euler(0f, 0f, 90f));
+                        Vector3 newPos = hit.point;
+                        Debug.Log($"Hit Point (rysowanie): {newPos}");
+
+                        // Jesli mamy zapisana poprzednia pozycje wypelniamy ewentualne luki
+                        if (lastDotPosition != Vector3.zero)
+                        {
+                            float distance = Vector3.Distance(newPos, lastDotPosition);
+                            if (distance > maxDotSpacing)
+                            {
+                                int numInterpolated = Mathf.FloorToInt(distance / maxDotSpacing);
+                                for (int i = 1; i <= numInterpolated; i++)
+                                {
+                                    // interpolacja liniowa miedzy lastDotPosition a newPos
+                                    float t = (float)i / (numInterpolated + 1);
+                                    Vector3 interpPos = Vector3.Lerp(lastDotPosition, newPos, t);
+                                    GameObject interpDot = Instantiate(dot, interpPos, Quaternion.Euler(0f, 0f, 90f));
+                                    if (pathManager != null)
+                                        pathManager.AddDot(interpDot);
+                                    else
+                                        Debug.LogWarning("PathManager jest null!");
+                                }
+                            }
+                        }
+
+                        // dodajemy glowna kropke na nowej pozycji
+                        GameObject newDot = Instantiate(dot, newPos, Quaternion.Euler(0f, 0f, 90f));
                         if (pathManager != null)
                             pathManager.AddDot(newDot);
+
+
+                        // aktualizacja pozycji ostatniej kropki
+                        lastDotPosition = newPos;
                     }
-                }
-                else
-                {
-                    Debug.Log("No hit");
                 }
             }
         }
         else
         {
-            // tryb kolorowania
+            //tryb kolorowania
+            //Debug.Log("Tryb kolorowania wlaczony");
+            int dotsLayer = LayerMask.NameToLayer("Dots");
+            int dotsMask = 1 << dotsLayer;
+            rayInteractor.raycastMask = dotsMask;
+
+
+
+            if (isHovering)
+            {
+                if (primaryButtonAction.action.ReadValue<float>() > 0)
+                {
+                    if (rayInteractor.TryGetCurrent3DRaycastHit(out RaycastHit hit))
+                    {
+                        Debug.Log($"Hit Point (kolorowanie): {hit.point} - Trafiono w: {hit.collider.gameObject.name}");
+                        DotRecolor dotRecolor = hit.collider.GetComponent<DotRecolor>();
+                        if (dotRecolor != null)
+                        {
+                            Debug.Log("Klikniêto kropkê o indeksie: " + dotRecolor.dotIndex);
+                            dotRecolor.Recolor();
+                        }
+                        else
+                        {
+                            Debug.Log("Trafiono obiekt, ktory nie jest kropka.");
+                        }
+                    }
+                    else
+                    {
+                        Debug.Log("Laser nie trafi³ w zaden obiekt.");
+                    }
+                }
+            }
         }
     }
-
 
     public void OnHoverEntered(HoverEnterEventArgs args)
     {
         isHovering = true;
+        //Debug.Log("isHovering ustawione na true");
     }
+
     public void OnHoverExit(HoverExitEventArgs args)
     {
         isHovering = false;
+        //Debug.Log("isHovering ustawione na false");
     }
 }
